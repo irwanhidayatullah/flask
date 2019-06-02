3ca0cc751b497ec35af404071b745bf9e188d9a2
irwanhidayatullah <irwanhidayatullah@gmail.com>
2019-06-02 14:31:57 +0700

    add request post dan get

--- form-login/app.py
+++ form-login/app.py
@@ -1,15 +1,27 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, make_response, session
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+    # return redirect('/login')
+
     
 
-@app.route('/login')
+@app.route('/login', methods=["GET","POST"])
 def login():
+    if request.method == 'POST':
+        resp = make_response('ini adalah respon ' + request.form['uname'])
+        resp.set_cookie('email_kamu', request.form['uname'])
+        return resp
+    
     return render_template('login.html')
+
+@app.route('/cokies')
+def cokies():
+    email = str(request.cookies.get('email_kamu'))
+    return 'ini adalah cookies yang tersimpan' + email
 
 
 if __name__=="__main__":
